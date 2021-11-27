22f7213bac1d0fe38208c99bc1669b47d47828e6
AnhNhut2639 <zoro92211@gmail.com>
2021-11-27 17:08:33 +0700

    refactor comment area on newsfees

---
+++ frontend/src/components/newFeeds/Comment.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import Picker from "emoji-picker-react";
+import { FaIcons } from "react-icons/fa";
+import axios from "axios";
+
+function Comment(props) {
+  const { id } = props; // idPost
+  const [showPicker, setShowPicker] = useState(false);
+  const [cmt, setCmt] = useState("");
+  const onEmojiClick = (event, emojiObject) => {
+    setCmt(cmt + emojiObject.emoji);
+  };
+
+  useEffect(() => {
+    window.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        setShowPicker(false);
+      }
+    });
+  });
+  const handlePostCmt = () => {
+    if (cmt === "") {
+      alert("Nhập nội dung bình luận");
+      return;
+    } else {
+      axios.post(
+        "http://localhost:3001/user/cmt",
+        {
+          idPost: id,
+          content: cmt,
+        },
+        { withCredentials: true }
+      );
+      setCmt("");
+    }
+  };
+
+  const handleOnchange = (e) => {
+    const val = e.target.value;
+    setCmt(val);
+  };
+  const handleOff = () => {
+    setShowPicker(false);
+  };
+
+  return (
+    <React.Fragment>
+      <FaIcons
+        className="faIcon"
+        onClick={() => setShowPicker((val) => !val)}
+      />
+      <input
+        type="text"
+        value={cmt}
+        className="input-style"
+        onChange={handleOnchange}
+        placeholder="Add a comment..."
+        onClick={handleOff}
+      />
+      {showPicker && (
+        <Picker pickerStyle={{ width: "100%" }} onEmojiClick={onEmojiClick} />
+      )}
+      <span onClick={() => handlePostCmt(id)}>post</span>
+    </React.Fragment>
+  );
+}
+
+export default Comment;
--- frontend/src/components/newFeeds/MiddleColumn.js
+++ frontend/src/components/newFeeds/MiddleColumn.js
@@ -5,6 +5,7 @@ import InputEmoji from "react-input-emoji";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import Cookies from "js-cookie";
+import Comment from "./Comment";
 import {
   FaHeart,
   FaRegHeart,
@@ -17,7 +18,6 @@ function MiddleColumn(props) {
   const currentUser = jwt.decode(token, process.env.REACT_APP_MY_SERECT_KEY);
   const idCurrentUser = currentUser.payload.id;
 
-  const [cmt, setCmt] = useState("");
   const [newsFeeds, setNewsFeeds] = useState([]);
   const [dataCmt, setDataCmt] = useState([]);
 
@@ -43,24 +43,7 @@ function MiddleColumn(props) {
       .catch(function (error) {
         console.log(error);
       });
-  }, []);
-  const postComments = (idPost) => {
-    if (cmt === "") {
-      alert("Nhập nội dung bình luận");
-      return;
-    } else {
-      axios.post(
-        "http://localhost:3001/user/cmt",
-        {
-          idPost: idPost,
-          content: cmt,
-        },
-        { withCredentials: true }
-      );
-      setCmt("");
-    }
-  };
-
+  }, [dataCmt]);
   const viewpost = (image, avatar, uname, status, idPost) => {
     document.getElementById("view").style.display = "flex";
 
@@ -300,27 +283,7 @@ function MiddleColumn(props) {
                 <div className="dayPost">{news.datePosted}</div>
               </div>
               <div className="commentary">
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="audio-description"
-                  className="svg-inline--fa fa-audio-description fa-w-16"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M162.925 238.709l8.822 30.655h-25.606l9.041-30.652c1.277-4.421 2.651-9.994 3.872-15.245 1.22 5.251 2.594 10.823 3.871 15.242zm166.474-32.099h-14.523v98.781h14.523c29.776 0 46.175-17.678 46.175-49.776 0-32.239-17.49-49.005-46.175-49.005zM512 112v288c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48zM245.459 336.139l-57.097-168A12.001 12.001 0 0 0 177 160h-35.894a12.001 12.001 0 0 0-11.362 8.139l-57.097 168C70.003 343.922 75.789 352 84.009 352h29.133a12 12 0 0 0 11.535-8.693l8.574-29.906h51.367l8.793 29.977A12 12 0 0 0 204.926 352h29.172c8.22 0 14.006-8.078 11.361-15.861zm184.701-80.525c0-58.977-37.919-95.614-98.96-95.614h-57.366c-6.627 0-12 5.373-12 12v168c0 6.627 5.373 12 12 12H331.2c61.041 0 98.96-36.933 98.96-96.386z"
-                  ></path>
-                </svg>
-                <InputEmoji
-                  onChange={setCmt}
-                  cleanOnEnter
-                  placeholder="Type a comment"
-                />
-                <span onClick={() => postComments(news.idPost)}>Post</span>
+                <Comment id={news.idPost} />
               </div>
             </div>
           );
