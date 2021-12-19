7ab8e64df3acd30023da9f25e781bfc0f2d91601
AnhNhut2639 <zoro92211@gmail.com>
2021-12-19 21:33:48 +0700

    added feature logout and refactor header

--- frontend/src/components/newFeeds/RightColumn.js
+++ frontend/src/components/newFeeds/RightColumn.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import jwt from "jsonwebtoken";
 import Cookies from "js-cookie";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function RightColumn(props) {
   const [users, setUsers] = useState([]);
   const [person, setPerson] = useState([]);
   const token = Cookies.get("accessToken");
+  let history = useHistory();
 
   const result = jwt.decode(token, process.env.REACT_APP_MY_SERECT_KEY);
   let id = result.payload.id;
@@ -34,6 +36,16 @@ function RightColumn(props) {
       });
   }, []);
 
+  const handleLogout = () => {
+    let answer = window.confirm("Are you sure you want to switch Account ?");
+    if (answer) {
+      Cookies.remove("accessToken");
+      history.push("/login");
+    } else {
+      return;
+    }
+  };
+
   return (
     <React.Fragment>
       <div className="right-column">
@@ -46,7 +58,9 @@ function RightColumn(props) {
               <div className="account-username">{person.fullname}</div>
               <div className="account-name">{person.username}</div>
             </div>
-            <div className="switch">Switch</div>
+            <div className="switch" onClick={handleLogout}>
+              Switch
+            </div>
           </div>
           <div className="suggest4-you">
             <span>Suggest for you</span>
